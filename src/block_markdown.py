531242ba59cfa19ed1334b71df86d55814ad8a45
srcct3 <src.ct3@gmail.com>
2025-10-10 12:42:42 +0300

    CH4L2: Block Types

---
+++ src/block_markdown.py
@@ -0,0 +1,53 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+
+def get_heading(block):
+    count = 0
+    while block[count] == "#":
+        count += 1
+    if count > 6:
+        return BlockType.PARAGRAPH
+    return BlockType.HEADING
+
+
+def markdown_to_block(markdown: str):
+    blocks = []
+    for block in markdown.split("\n\n"):
+        striped = block.strip()
+        if striped != "":
+            blocks.append(striped)
+
+    return blocks
--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -75,6 +75,15 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 def extract_markdown_images(text):
     pattern = r'!\[([^\]]+?)\]\(([^)]+?)\)'
     matches = re.findall(pattern, text)
